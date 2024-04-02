4b0f73886675e4da5f09745312931036565ea9d6
악덕 <rudejrqkr97@gmail.com>
2024-04-02 17:25:48 +0900

    :tada: init

---
+++ src/main/java/akatsuki/moodholic/MoodHolicApplication.java
@@ -0,0 +1,13 @@
+package akatsuki.moodholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodHolicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodHolicApplication.class, args);
+	}
+
+}

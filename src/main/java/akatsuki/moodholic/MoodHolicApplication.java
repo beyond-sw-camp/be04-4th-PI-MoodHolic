7bc98ea157c0bc6f560d3f326c38541692bdb758
악덕 <rudejrqkr97@gmail.com>
2024-04-03 17:27:26 +0900

    feat(openai): OpenAI API prompt
    - MoodHolic 프로젝트에 롬복 구현
    - 새로운 클래스 'TomcatWebCustomConfig' 생성
    - 새로운 클래스 'Message' 및 'ChatGPTResponse' 생성
    - 새로운 클래스 'ChatGPTRequest' 생성
    - 새로운 컨트롤러 'ChatGPTController' 생성
    - 새로운 설정 클래스 'ChatGPTConfig' 생성

--- src/main/java/akatsuki/moodholic/MoodHolicApplication.java
+++ src/main/java/akatsuki/moodholic/MoodHolicApplication.java
@@ -2,12 +2,24 @@ package akatsuki.moodholic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class MoodHolicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoodHolicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoodHolicApplication.class, args);
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new
+                CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+
+    }
 
 }

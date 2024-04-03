026a1e31f594640443f4fd1f0bcf97570f50dd73
daersh <daersh@naver.com>
2024-04-03 20:42:11 +0900

    feet(image): 이미지 업로드 기능 구현
    - 이미지 업로드 받을 이미지 rest-controller 구현 및 작동 확인
    - 이미지 파일을 구글 클라우드로 업로드하기 위한 이미지 서비스 구현 및 작동 확인
    - 이미지 업로드 요청 후 해당 이미지의 url 주소 출력 확인

---
+++ src/main/java/akatsuki/moodholic/controller/ImageController.java
@@ -0,0 +1,29 @@
+package akatsuki.moodholic.controller;
+
+import akatsuki.moodholic.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PatchMapping("/profile")
+    public ResponseEntity<String> get(@RequestParam("file") MultipartFile file) throws IOException {
+        System.out.println("profile upload request..");
+        String imageUrl = imageService.uploadImage(file);
+        System.out.println("imageUrl = " + imageUrl);
+        return ResponseEntity.status(HttpStatus.OK).body(imageUrl);
+    }
+}

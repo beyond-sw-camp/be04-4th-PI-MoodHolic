a405bd00377cb9e5a48919d568721d2d4c7146c6
악덕 <rudejrqkr97@gmail.com>
2024-04-04 16:04:14 +0900

    refactor: 폴조 구조 변경

--- src/main/java/akatsuki/moodholic/repository/EmotionDAO.java
+++
@@ -1,9 +0,0 @@
-package akatsuki.moodholic.repository;
-
-import akatsuki.moodholic.domain.Emotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmotionDAO  extends JpaRepository<Emotion,Integer> {
-}

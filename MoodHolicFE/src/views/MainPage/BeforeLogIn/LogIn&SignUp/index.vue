a0caef350e9c41871bb56e51b284cfa94303b53d
alu98@naver.com <alu98@naver.com>
2024-04-08 17:43:16 +0900

    init

---
+++ MoodHolicFE/src/views/MainPage/BeforeLogIn/LogIn&SignUp/index.vue
@@ -0,0 +1,90 @@
+<template>
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="/MoodHolicFE/src/assets/css/MainPage/BeforeLogIn/LogIn&SignUp/global.css" />
+    <link rel="stylesheet" href="/MoodHolicFE/src/assets/css/MainPage/BeforeLogIn/LogIn&SignUp/index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="div">
+      <header class="line-parent">
+        <div class="frame-child"></div>
+        <img
+          class="moodholiclogo-2-icon"
+          loading="lazy"
+          alt=""
+          src="/MoodHolicFE/src/assets/img/MainPage/BeforeLogIn/LogIn&SignUp/moodholiclogo-2@2x.png"
+        />
+      </header>
+      <section class="inner">
+        <div class="rectangle-parent">
+          <div class="frame-item"></div>
+          <div class="invitation-message">
+            <h2 class="h2">
+              <p class="p">오늘도 당신의</p>
+              <p class="p1">기분을 듣고 싶습니다!</p>
+            </h2>
+          </div>
+          <div class="mood-holic">
+            <div class="mood-holic-wrapper">
+              <h1 class="mood-holic1">Mood Holic</h1>
+            </div>
+            <div class="mood-holic-child"></div>
+          </div>
+          <div class="frame-wrapper">
+            <div class="frame-parent">
+              <div class="existing-account-parent">
+                <div class="existing-account">
+                  <div class="previously-registered-users">
+                    <b class="b">기존 계정으로 로그인</b>
+                  </div>
+                  <button class="rectangle-group">
+                    <div class="frame-inner"></div>
+                    <img
+                      class="group-icon"
+                      alt=""
+                      src="/MoodHolicFE/src/assets/img/MainPage/BeforeLogIn/LogIn&SignUp/group-16.svg"
+                    />
+
+                    <div class="log-in-with">Log in with Google</div>
+                  </button>
+                </div>
+                <button class="not-member-message">
+                  <div class="not-member-message-child"></div>
+                  <img
+                    class="join-message-icon"
+                    alt=""
+                    src="/MoodHolicFE/src/assets/img/MainPage/BeforeLogIn/LogIn&SignUp/카카오톡.png"
+                  />
+
+                  <div class="log-in-with1">Log in with Kakao</div>
+                </button>
+              </div>
+              <div class="sign-up-button">
+                <b class="b1">새 계정으로 로그인</b>
+              </div>
+            </div>
+          </div>
+          <div class="footer-area">
+            <div class="parent">
+              <b class="b2">아직 회원이 아니신가요?</b>
+              <b class="b3">회원가입</b>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
+</template>
+
+<script setup>
+
+</script>

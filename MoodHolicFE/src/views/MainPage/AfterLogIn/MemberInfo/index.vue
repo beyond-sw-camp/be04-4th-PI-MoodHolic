5062453a84ee329fe3b4c28996a0e4e32db42f90
alu98@naver.com <alu98@naver.com>
2024-04-09 10:52:44 +0900

    Design: #37 - MainPage memberInfo 설정창 init

---
+++ MoodHolicFE/src/views/MainPage/AfterLogIn/MemberInfo/index.vue
@@ -0,0 +1,183 @@
+<template>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="div">
+      <header class="member-info">
+        <div class="member-info-inner">
+          <div class="frame-parent">
+            <img
+              class="frame-child"
+              loading="lazy"
+              alt=""
+              src="./public/group-39.svg"
+            />
+
+            <img
+              class="moodholiclogo-2-icon"
+              loading="lazy"
+              alt=""
+              src="./public/moodholiclogo-2@2x.png"
+            />
+
+            <div class="frame-wrapper">
+              <div class="frame-group">
+                <div class="wrapper">
+                  <div class="div1">오늘 날씨</div>
+                </div>
+                <div class="frame-container">
+                  <div class="image-1-parent">
+                    <img
+                      class="image-1-icon"
+                      loading="lazy"
+                      alt=""
+                      src="./public/image-1@2x.png"
+                    />
+
+                    <div class="div2">14 ℃</div>
+                  </div>
+                </div>
+                <div class="group-parent">
+                  <img
+                    class="group-icon"
+                    loading="lazy"
+                    alt=""
+                    src="./public/group.svg"
+                  />
+
+                  <img
+                    class="group-icon1"
+                    alt=""
+                    src="./public/group-1@2x.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="member-info-child"></div>
+      </header>
+      <div class="inner">
+        <div class="frame-div">
+          <div class="frame-parent1">
+            <div class="container">
+              <h1 class="h1">로그인 정보</h1>
+            </div>
+            <div class="vector-parent">
+              <img class="frame-item" alt="" src="./public/rectangle-13.svg" />
+
+              <div class="frame">
+                <div class="div3">닉네임</div>
+              </div>
+              <div class="frame-parent2">
+                <div class="frame-wrapper1">
+                  <div class="konan-parent">
+                    <div class="konan">Konan</div>
+                    <div class="wrapper1">
+                      <div class="div4">변경하기</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="frame-inner"></div>
+              </div>
+              <div class="wrapper2">
+                <div class="div5">연결된 계정</div>
+              </div>
+              <div class="frame-wrapper2">
+                <div class="frame-parent3">
+                  <div class="frame-parent4">
+                    <img
+                      class="frame-child1"
+                      loading="lazy"
+                      alt=""
+                      src="./public/group-10@2x.png"
+                    />
+
+                    <div class="dlwldms0902gmailcom-wrapper">
+                      <div class="dlwldms0902gmailcom">
+                        dlwldms0902@gmail.com
+                      </div>
+                    </div>
+                  </div>
+                  <div class="wrapper3">
+                    <div class="div6">변경하기</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="frame-parent5">
+            <div class="wrapper4">
+              <h1 class="h11">내 기록</h1>
+            </div>
+            <div class="frame-parent6">
+              <div class="vector-group">
+                <img
+                  class="rectangle-icon"
+                  alt=""
+                  src="./public/rectangle-16.svg"
+                />
+
+                <div class="wrapper5">
+                  <b class="b">기록한 하루</b>
+                </div>
+                <div class="parent">
+                  <img
+                    class="icon"
+                    loading="lazy"
+                    alt=""
+                    src="./public/-20240405--218-1@2x.png"
+                  />
+
+                  <div class="wrapper6">
+                    <b class="b1">12개</b>
+                  </div>
+                </div>
+              </div>
+              <div class="vector-container">
+                <img
+                  class="frame-child2"
+                  alt=""
+                  src="./public/rectangle-17.svg"
+                />
+
+                <div class="wrapper7">
+                  <b class="b2">좋아요 글</b>
+                </div>
+                <div class="vector-parent1">
+                  <img
+                    class="vector-icon"
+                    loading="lazy"
+                    alt=""
+                    src="./public/vector.svg"
+                  />
+
+                  <div class="wrapper8">
+                    <b class="b3">3개</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
+</template>
+
+<script setup>
+
+</script>

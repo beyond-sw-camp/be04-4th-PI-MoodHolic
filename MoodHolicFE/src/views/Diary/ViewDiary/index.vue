b94c1aa032b4f8af3ce37ea847c9b1b2e960ff8e
alu98@naver.com <alu98@naver.com>
2024-04-09 14:40:57 +0900

    Design: #49 - 다이어리 조회 init

---
+++ MoodHolicFE/src/views/Diary/ViewDiary/index.vue
@@ -0,0 +1,378 @@
+<template>
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="ai">
+      <header class="ai-inner">
+        <div class="frame-parent">
+          <img
+            class="frame-child"
+            loading="lazy"
+            alt=""
+            src="./public/group-39.svg"
+          />
+
+          <img
+            class="moodholiclogo-2-icon"
+            loading="lazy"
+            alt=""
+            src="./public/moodholiclogo-2@2x.png"
+          />
+
+          <div class="frame-wrapper">
+            <div class="frame-group">
+              <div class="wrapper">
+                <div class="div">오늘 날씨</div>
+              </div>
+              <div class="dot-symbol">
+                <div class="image-1-parent">
+                  <img
+                    class="image-1-icon"
+                    loading="lazy"
+                    alt=""
+                    src="./public/image-1@2x.png"
+                  />
+
+                  <div class="div1">14 ℃</div>
+                </div>
+              </div>
+              <div class="media-label">
+                <img
+                  class="group-icon"
+                  loading="lazy"
+                  alt=""
+                  src="./public/group.svg"
+                />
+
+                <img class="group-icon1" alt="" src="./public/group-1@2x.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="r-b-name-wrapper">
+        <div class="r-b-name"></div>
+      </div>
+      <section class="like-symbol">
+        <div class="frame-container">
+          <div class="a-i-recommendation-label-wrapper">
+            <button class="a-i-recommendation-label">
+              <div class="a-i-recommendation-label-child"></div>
+              <div class="vector-wrapper">
+                <img class="vector-icon" alt="" src="./public/vector.svg" />
+              </div>
+              <div class="div2">뒤로가기</div>
+            </button>
+          </div>
+          <div class="vegetarian-or-nonvegetarian-la">
+            <div class="vegetarian-or-nonvegetarian-la-child"></div>
+            <div class="frame-div">
+              <div class="heart-symbol-parent">
+                <div class="heart-symbol"></div>
+                <div class="parent">
+                  <img
+                    class="icon"
+                    alt=""
+                    src="./public/-20240405--318@2x.png"
+                  />
+
+                  <img
+                    class="icon1"
+                    loading="lazy"
+                    alt=""
+                    src="./public/-20240405--405@2x.png"
+                  />
+                </div>
+              </div>
+              <div class="rectangle-parent">
+                <div class="frame-item"></div>
+                <div class="frame-wrapper1">
+                  <div class="frame-parent1">
+                    <img
+                      class="frame-inner"
+                      loading="lazy"
+                      alt=""
+                      src="./public/group-143.svg"
+                    />
+
+                    <div class="container">
+                      <div class="div3">2024년 4월 5일 금요일</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="line-parent">
+                  <div class="line-div"></div>
+                  <div class="frame-wrapper2">
+                    <div class="fluentbook-number-24-filled-parent">
+                      <img
+                        class="fluentbook-number-24-filled-icon"
+                        loading="lazy"
+                        alt=""
+                        src="./public/fluentbooknumber24filled.svg"
+                      />
+
+                      <div class="frame">
+                        <div class="div4">5점</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="rectangle-group">
+              <div class="rectangle-div"></div>
+              <div class="frame-parent2">
+                <div class="group">
+                  <img
+                    class="icon2"
+                    loading="lazy"
+                    alt=""
+                    src="./public/-20240405--218-1@2x.png"
+                  />
+
+                  <div class="wrapper1">
+                    <div class="div5">오늘의 기분</div>
+                  </div>
+                </div>
+                <div class="wrapper2">
+                  <div class="div6">오늘 내 기분을 말하자면 마이쮸같았지</div>
+                </div>
+              </div>
+              <div class="daily-story">
+                <img
+                  class="icon3"
+                  loading="lazy"
+                  alt=""
+                  src="./public/-20240405--218-2@2x.png"
+                />
+
+                <div class="wrapper3">
+                  <div class="div7">하루 이야기</div>
+                </div>
+              </div>
+              <div class="classroom-prep">
+                <div class="div8">
+                  <p class="p">
+                    오늘 학원에서 프로젝트 준비하고 화면 디자인 하는데 시간을
+                  </p>
+                  <p class="p1">
+                    다썼다. 강의실이 너무 추워서 심장이 마이쮸같이
+                  </p>
+                  <p class="p2">
+                    쫄깃 쫄깃 해지는 것 같다. 반팔입은 사람들은 안 추운걸꽈,,,?
+                  </p>
+                  <p class="p3">
+                    집에는 언제 가쥐? 내일은 이사가는 날이라 집가서짐을 싸야할
+                    것
+                  </p>
+                  <p class="p4">같다.</p>
+                </div>
+              </div>
+            </div>
+            <div class="music-media">
+              <div class="food-preference">
+                <div class="frame-parent3">
+                  <div class="frame-wrapper3">
+                    <div class="frame-parent4">
+                      <div class="vector-container">
+                        <img
+                          class="vector-icon1"
+                          alt=""
+                          src="./public/vector-1.svg"
+                        />
+                      </div>
+                      <h2 class="ai-pick">AI Pick</h2>
+                    </div>
+                  </div>
+                  <div class="frame-parent5">
+                    <div class="frame-wrapper4">
+                      <div class="rectangle-container">
+                        <div class="frame-child1"></div>
+                        <div class="div9">음식</div>
+                      </div>
+                    </div>
+                    <div class="frame-parent6">
+                      <div class="group-div">
+                        <div class="frame-child2"></div>
+                        <div class="miyeokguk-hanjeong">
+                          <img
+                            class="vector-icon2"
+                            alt=""
+                            src="./public/vector-2.svg"
+                          />
+
+                          <div class="food-story">
+                            <div class="div10">미역국</div>
+                          </div>
+                        </div>
+                        <div class="kim-buphoo-parent">
+                          <img
+                            class="kim-buphoo-icon"
+                            alt=""
+                            src="./public/vector-3.svg"
+                          />
+
+                          <div class="wrapper4">
+                            <div class="div11">1</div>
+                          </div>
+                        </div>
+                        <div class="miyeokguk-hanjeong1">
+                          <img
+                            class="vector-icon3"
+                            alt=""
+                            src="./public/vector-4.svg"
+                          />
+
+                          <div class="wrapper5">
+                            <div class="div12">한식</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="a-i-recommendations">
+                        <img
+                          class="material-symbolsheart-plus-ou-icon"
+                          loading="lazy"
+                          alt=""
+                          src="./public/materialsymbolsheartplusoutline.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="frame-parent7">
+                  <div class="frame-wrapper5">
+                    <div class="rectangle-parent1">
+                      <div class="frame-child3"></div>
+                      <div class="div13">음악</div>
+                    </div>
+                  </div>
+                  <div class="frame-parent8">
+                    <div class="rectangle-parent2">
+                      <div class="frame-child4"></div>
+                      <div class="kim-bok-joo-wrapper">
+                        <div class="kim-bok-joo">
+                          <img
+                            class="group-icon2"
+                            alt=""
+                            src="./public/group-2.svg"
+                          />
+
+                          <div class="wrapper6">
+                            <div class="div14">21</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="r-b-label-wrapper">
+                        <div class="r-b-label">
+                          <img
+                            class="group-icon3"
+                            alt=""
+                            src="./public/group-3.svg"
+                          />
+
+                          <div class="dean-wrapper">
+                            <div class="dean">DEAN</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="vector-parent">
+                        <img
+                          class="vector-icon4"
+                          alt=""
+                          src="./public/vector-5.svg"
+                        />
+
+                        <div class="rb-wrapper">
+                          <div class="rb">R&B</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="material-symbolsheart-plus-ou-wrapper">
+                      <img
+                        class="material-symbolsheart-plus-ou-icon1"
+                        alt=""
+                        src="./public/materialsymbolsheartplusoutline-1.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="frame-parent9">
+                  <div class="frame-wrapper6">
+                    <div class="rectangle-parent3">
+                      <div class="frame-child5"></div>
+                      <div class="div15">영화</div>
+                    </div>
+                  </div>
+                  <div class="frame-parent10">
+                    <div class="rectangle-parent4">
+                      <div class="frame-child6"></div>
+                      <div class="frame-wrapper7">
+                        <div class="vector-group">
+                          <img
+                            class="vector-icon5"
+                            alt=""
+                            src="./public/vector-6.svg"
+                          />
+
+                          <div class="div16">기생충</div>
+                        </div>
+                      </div>
+                      <div class="vector-parent1">
+                        <img
+                          class="vector-icon6"
+                          alt=""
+                          src="./public/vector-7.svg"
+                        />
+
+                        <div class="love-heart">
+                          <div class="div17">봉준호</div>
+                        </div>
+                      </div>
+                      <div class="frame-wrapper8">
+                        <div class="vector-parent2">
+                          <img
+                            class="vector-icon7"
+                            alt=""
+                            src="./public/vector-8.svg"
+                          />
+
+                          <div class="food-items">
+                            <div class="div18">드라마</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="material-symbolsheart-plus-ou-container">
+                      <img
+                        class="material-symbolsheart-plus-ou-icon2"
+                        alt=""
+                        src="./public/materialsymbolsheartplusoutline-2.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
+
+</template>
+
+<script setup>
+
+</script>

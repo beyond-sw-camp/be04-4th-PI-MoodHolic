d442a6454637e100e94b82406dcc8429cecac861
daersh <daersh@naver.com>
2024-04-11 21:13:55 +0900

    [BE] refactor: 달력 리턴 객체 수정
    - 간략한 설명을 보이기 위해 Calendar 객체에 감정, 한줄요약, 내용 추가

--- MoodHolicBE/src/main/java/akatsuki/moodholic/service/facade/CalendarFacadeService.java
+++ MoodHolicBE/src/main/java/akatsuki/moodholic/service/facade/CalendarFacadeService.java
@@ -3,33 +3,45 @@ package akatsuki.moodholic.service.facade;
 import akatsuki.moodholic.domain.Diary;
 import akatsuki.moodholic.dto.Calendar;
 import akatsuki.moodholic.service.CalendarService;
+import akatsuki.moodholic.service.DiaryEmotionService;
 import akatsuki.moodholic.service.DiaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class CalendarFacadeService {
     private final CalendarService calendarService;
     private final DiaryService diaryService;
+    private final DiaryEmotionService diaryEmotionService;
 
     @Autowired
-    public CalendarFacadeService(CalendarService calendarService, DiaryService diaryService) {
+    public CalendarFacadeService(CalendarService calendarService, DiaryService diaryService, DiaryEmotionService diaryEmotionService) {
         this.calendarService = calendarService;
         this.diaryService = diaryService;
+        this.diaryEmotionService = diaryEmotionService;
     }
 
     public List<Calendar> getCalendar(long memberId) {
-        return calendarService.getCalendar(getDiaries(memberId));
+        List<Diary> diaries = getDiaries(memberId);
+        HashMap<Integer, Integer> emotions = getIntegerIntegerHashMap(diaries);
+        return calendarService.getCalendar(diaries,emotions);
     }
     public List<Calendar> getCalendarOfYear(long memberId, int year) {
-        return calendarService.getCalendarOfYear(getDiaries(memberId),year);
+        List<Diary> diaries = getDiaries(memberId);
+        HashMap<Integer, Integer> emotions = getIntegerIntegerHashMap(diaries);
+        return calendarService.getCalendarOfYear(getDiaries(memberId),year,emotions);
+    }
+
+    private HashMap<Integer, Integer> getIntegerIntegerHashMap(List<Diary> diaries) {
+        HashMap<Integer,Integer> emotions = diaryEmotionService.getEmotionMap(diaries);
+        return emotions;
     }
 
     private List<Diary> getDiaries(long memberId) {
-        List<Diary> diaryList = diaryService.getMemberDiaries(memberId);
-        return diaryList;
+        return diaryService.getMemberDiaries(memberId);
     }
 
 }

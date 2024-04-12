6d875d8ddfd9d4844344cda96ee50bf61ebae7f5
cookeeholic <cookeeholic@naver.com>
2024-04-12 20:28:51 +0900

    [FE] Feat: router  추가

--- MoodHolicFE/src/main.js
+++ MoodHolicFE/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router/index.js";
 
-createApp(App).mount('#app')
+// Vue 애플리케이션 생성
+const app = createApp(App)
+
+// Vue 라우터 및 Vuex 스토어를 애플리케이션에 연결
+app.use(router)
+
+// 애플리케이션을 DOM에 마운트
+app.mount('#app')

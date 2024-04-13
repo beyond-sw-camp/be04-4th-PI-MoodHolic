73eb93161843bba1a6f1515499dbb0f14c03545e
daersh <daersh@naver.com>
2024-04-13 16:18:50 +0900

    [BE] Add: 멤버의 영화 장르 랭킹 기능 추가
    - 멤버가 좋아요한 영화 리스트의 카테고리를 분석하여 랭킹으로 반환하는 기능

---
+++ MoodHolicBE/src/main/java/akatsuki/moodholic/dto/MemberMovieGenreRanking.java
@@ -0,0 +1,22 @@
+package akatsuki.moodholic.dto;
+
+import lombok.Data;
+
+import java.util.HashMap;
+
+@Data
+public class MemberMovieGenreRanking {
+    private HashMap<String, Integer> movieRankings;
+    private String topName;
+    private int topCnt;
+
+    public MemberMovieGenreRanking() {
+        movieRankings = new HashMap<>();
+    }
+
+    public MemberMovieGenreRanking(HashMap<String, Integer> lists, int maximum, String name) {
+        movieRankings=lists;
+        topName=name;
+        topCnt=maximum;
+    }
+}

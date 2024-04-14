9a007d4ca62d34c8e8d230b1a7be70374b150f83
악덕 <rudejrqkr97@gmail.com>
2024-04-14 17:29:58 +0900

    feat(App.vue): HeaderComponent와 LogIn 컴포넌트 추가
    - Header 컴포넌트를 추가하여 헤더를 표시함
    - LogIn 컴포넌트를 추가하여 로그인 기능을 구현함

--- MoodHolicFE/src/App.vue
+++ MoodHolicFE/src/App.vue
@@ -1,40 +1,12 @@
 <template>
-<sign-up></sign-up>
-<before-log-in-main></before-log-in-main>
-<after-log-in-main></after-log-in-main>
-<log-in></log-in>
-<welcome></welcome>
-<card></card>
-<list></list>
-<month></month>
-<year></year>
-<preview></preview>
-<view></view>
-<write></write>
-<info></info>
-<block-graph></block-graph>
-<line-graph></line-graph>
+  <header><HeaderComponent/></header>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <script setup>
-import SignUp from "@/components/MainPage/SighUp/SignUp.vue";
-import BeforeLogInMain from "@/components/MainPage/BeforeLogInMain/BeforeLogInMain.vue";
-import AfterLogInMain from "@/components/MainPage/AfterLogInMain/AfterLogInMain.vue";
-import LogIn from "@/components/MainPage/LogIn/LogIn.vue";
-import Welcome from "@/components/MainPage/Welcome/Welcome.vue";
-import Card from "@/components/Profile/AIrecommended/Card/Card.vue";
-import List from "@/components/Profile/AIrecommended/List/List.vue";
-import Month from "@/components/Profile/Calendar/Month/Month.vue";
-import Year from "@/components/Profile/Calendar/Year/Year.vue";
-import Preview from "@/components/Profile/Diary/Preview/Preview.vue";
-import View from "@/components/Profile/Diary/View/View.vue";
-import Write from "@/components/Profile/Diary/Write/Write.vue";
-import Info from "@/components/Profile/Info/Info.vue";
-import BlockGraph from "@/components/Profile/Statistics/BlockGraph/BlockGraph.vue";
-import LineGraph from "@/components/Profile/Statistics/LineGraph/LineGraph.vue";
 
+import LogIn from "@/components/MainPage/Login/LogIn.vue";
+import HeaderComponent from "@/components/Header/Header.vue";
 </script>
-
-<style scoped>
-
-</style>

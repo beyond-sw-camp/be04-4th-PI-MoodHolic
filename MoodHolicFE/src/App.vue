216b131197c00b37c6744283208bbd1ba5fb1dfd
alu98@naver.com <alu98@naver.com>
2024-04-15 01:08:56 +0900

    refactor: #62 - 캘린더 월별, 년별 조회 창 아이콘 변경 및 경로 수정

--- MoodHolicFE/src/App.vue
+++ MoodHolicFE/src/App.vue
@@ -5,15 +5,17 @@
 
 <!--    <Card/>-->
 <!--  <List/>-->
-<!--<Month/>-->
 <!--  <View/>-->
+<!--<Month/>-->
+  <Year/>
   <Preview/>
 </template>
 
 <script setup>
   import Header from "@/components/Header/Header.vue";
   // import View from "@/components/Profile/Diary/View/View.vue";
-  import Preview from "@/components/Profile/Diary/Preview/Preview.vue";
+  // import Preview from "@/components/Profile/Diary/Preview/Preview.vue";
+  import Year from "@/components/Profile/Calendar/Year/Year.vue";
   // import List from "@/components/Profile/AIrecommended/List/List.vue";
   // import Card from "@/components/Profile/AIrecommended/Card/Card.vue";
   // import Month from "@/components/Profile/Calendar/Month/Month.vue";

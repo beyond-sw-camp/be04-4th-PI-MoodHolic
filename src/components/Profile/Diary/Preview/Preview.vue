1b5a2ca20e4733b44601b1b6dc6d8682b3741c30
alu98@naver.com <alu98@naver.com>
2024-04-16 18:33:51 +0900

    Design: #77 - 전체적인 css 수정작업

--- src/components/Profile/Diary/Preview/Preview.vue
+++ src/components/Profile/Diary/Preview/Preview.vue
@@ -206,7 +206,7 @@
 </script>
 
 <style>
-  @import "@/assets/css/Profile/Diary/Preview/Preview.css";
-  @import "@/assets/css/Profile/Diary/Preview/global.css";
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+@import "@/assets/css/Profile/Diary/Preview/Preview.css";
+@import "@/assets/css/Profile/Diary/Preview/global.css";
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 </style>
